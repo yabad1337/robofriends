3b2d99d5f64b815e49ac2e28135bb5b64cad03ff
Yahya ABAD <yabad@Yahyas-MacBook-Air.local>
2022-06-04 13:31:19 +0100

    render all elements from robots.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import 'tachyons';
+import { robots } from './robots';
+import CardList from './cardlist';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <CardList robots={robots}/>
   </React.StrictMode>
 );
 
